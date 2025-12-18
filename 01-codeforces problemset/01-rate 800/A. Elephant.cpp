1ec348ef0d34465038c9c4a48f46bdca7dc1ea70
Omar <ahlawyomar95@gmail.com>
2025-12-18 02:46:42 +0200

    add

---
+++ 01-codeforces problemset/01-rate 800/A. Elephant.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    long long x;
+    cin >> x;
+    if (x % 5 == 0) {
+        cout << x / 5;
+    }else 
+        cout << (x / 5) + 1;
+}
